6b7ddff567a9e54b61370f3c246bed5e65c666eb
takeclassic <takeclassic@naver,com>
2025-05-10 11:03:07 +0900

    resource 추가

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "341716572"
+    serviceWorkerVersion: "2054898560"
   }
 });
