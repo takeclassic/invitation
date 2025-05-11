56a6dc31f9810d4ced03e94ff403e6a89dbfd92a
takeclassic <takeclassic@naver,com>
2025-05-11 22:33:11 +0900

    서체 및 디자인 변경

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3944514452"
+    serviceWorkerVersion: "865892795"
   }
 });
