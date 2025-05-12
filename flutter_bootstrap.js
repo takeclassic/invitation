9f761a5d6182ff2218fe32c9c0c5a72700033691
takeclassic <takeclassic@naver,com>
2025-05-12 21:45:28 +0900

    수정

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "71631090"
+    serviceWorkerVersion: "4273696814"
   }
 });
