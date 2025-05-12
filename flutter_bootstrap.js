5e74eeb4a96d76c637520c04c09b108b4aceb6e5
takeclassic <takeclassic@naver,com>
2025-05-12 21:01:26 +0900

    마음 전하실 곳 추가

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "500187732"
+    serviceWorkerVersion: "31065710"
   }
 });
